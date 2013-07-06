ecb39aa8629e5cb71bdeffd3e189c500dd7d2d39
asafge <asafge@gmail.com>
2013-07-06 23:15:44 +0300

    https for login

--- newsplus/extensions/NewsblurExtension/src/com/noinnion/android/newsplus/extension/newsblurplus/APICalls.java
+++ newsplus/extensions/NewsblurExtension/src/com/noinnion/android/newsplus/extension/newsblurplus/APICalls.java
@@ -41,7 +41,8 @@ public class APICalls {
 	}
 	
 	public static String API_URL_BASE = "http://www.newsblur.com/";
-	public static String API_URL_LOGIN = API_URL_BASE + "api/login/";
+	public static String API_URL_BASE_SECURE = "https://www.newsblur.com/";
+	public static String API_URL_LOGIN = API_URL_BASE_SECURE + "api/login/";
 	public static String API_URL_FOLDERS_AND_FEEDS = API_URL_BASE + "reader/feeds?flat=true";
 	public static String API_URL_FEED_SINGLE = API_URL_BASE + "reader/feed/";
 	public static String API_URL_MARK_STORY_AS_READ = API_URL_BASE + "reader/mark_story_as_read/";
