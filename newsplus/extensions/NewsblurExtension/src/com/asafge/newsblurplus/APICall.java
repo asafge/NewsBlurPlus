1b090c39d1be6f27e73589fb9a53a1993ad8b793
asafge <asafge@gmail.com>
2013-07-15 20:25:48 +0300

    Allow to set APICall url after creating the object

--- newsplus/extensions/NewsblurExtension/src/com/asafge/newsblurplus/APICall.java
+++ newsplus/extensions/NewsblurExtension/src/com/asafge/newsblurplus/APICall.java
@@ -11,31 +11,44 @@ import com.androidquery.callback.AjaxStatus;
 
 public class APICall {
 	
-	public String CallbackUrl;
 	public JSONObject Json;
 	public AjaxStatus Status;
 	private AjaxCallback<JSONObject> callback;
 	private AQuery aquery;
+	private String callbackUrl;
+	
+	// Empty constructor
+	public APICall(Context c) {
+		aquery = new AQuery(c);
+	}
 	
 	// Constructor
 	public APICall(String url,  Context c) {
 		aquery = new AQuery(c);
 		// Create the callback object
-		CallbackUrl = url;
+		createCallback(url, c);
+	}
+
+	// Create a callback object, before running sync
+	public void createCallback(String url, Context c) {
+		callbackUrl = url;
 		callback = new AjaxCallback<JSONObject>();
 		callback.header("User-Agent", Prefs.USER_AGENT);
 		String[] sessionID = Prefs.getSessionID(c);
 		callback.cookie(sessionID[0], sessionID[1]);
-		callback.url(CallbackUrl).type(JSONObject.class);
+		callback.url(callbackUrl).type(JSONObject.class);
 	}
 	
 	// Add a parameter to the call
 	public void addParam(String key, Object value) {
-		callback.param(key, value);
+		if (callback != null)
+			callback.param(key, value);
 	}
 	
 	// Run synchronous HTTP request and check for valid response
 	public boolean sync() {
+		if (callback == null)
+			return false;
 		try {
 			aquery.sync(callback);
 			Json = callback.getResult();
