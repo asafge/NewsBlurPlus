f38fb9b12cf881306b4b7166483707b1ec681fe4
asafge <asafge@gmail.com>
2013-07-27 22:00:55 +0300

    Cleanups and comments

--- newsplus/extensions/NewsblurExtension/src/com/asafge/newsblurplus/LoginActivity.java
+++ newsplus/extensions/NewsblurExtension/src/com/asafge/newsblurplus/LoginActivity.java
@@ -16,7 +16,7 @@ import com.noinnion.android.reader.api.ReaderExtension;
 
 public class LoginActivity extends Activity implements OnClickListener {
 	
-	protected ProgressDialog	mBusy;
+	protected ProgressDialog mBusy;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,10 @@ public class LoginActivity extends Activity implements OnClickListener {
 		
 		String action = getIntent().getAction();
 		if (action != null && action.equals(ReaderExtension.ACTION_LOGOUT)) {
-			logout();
+			Prefs.setLoggedIn(c, false);
+			Prefs.setSessionID(c, "", "");
+			setResult(ReaderExtension.RESULT_LOGOUT);
+			finish();
 		}
 		if (Prefs.isLoggedIn(c)) {
 			setResult(ReaderExtension.RESULT_LOGIN);
@@ -35,32 +38,6 @@ public class LoginActivity extends Activity implements OnClickListener {
 		else {
 			setContentView(R.layout.login_newsblur);
 			findViewById(R.id.ok_button).setOnClickListener(this);
-		}
-	}
-
-	// Function for logging out - remove cookie
-	private void logout() {
-		final Context c = getApplicationContext();
-		Prefs.setLoggedIn(c, false);
-		Prefs.setSessionID(c, "", "");
-		setResult(ReaderExtension.RESULT_LOGOUT);
-		finish();
-	}
-	
-	// Login function, saves cookie when login is successful
-	private void login(String user, String pass) {
-		final Context c = getApplicationContext();
-		APICall ac = new APICall(APICall.API_URL_LOGIN, c);
-		ac.addPostParam("username", user);
-		ac.addPostParam("password", pass);
-		if (ac.sync()) {
-			Prefs.setSessionID(c, ac.Status.getCookies().get(0).getName(), ac.Status.getCookies().get(0).getValue());
-			Prefs.setLoggedIn(c, true);
-			setResult(ReaderExtension.RESULT_LOGIN);
-			finish();
-		}
-		else {
-			Toast.makeText(c, "Error:" + ac.Status.getMessage(), Toast.LENGTH_LONG).show();
 		}
 	}
 	
@@ -79,11 +56,13 @@ public class LoginActivity extends Activity implements OnClickListener {
 	}
 	
 	private class LoginTask extends AsyncTask<String, Void, Boolean> {
-
+		
+		// Show the login... process dialog
 		protected void onPreExecute() {
 			mBusy = ProgressDialog.show(LoginActivity.this, null, getText(R.string.msg_login_running), true, true);
 		}
 
+		// Async call to NewsBlur API for authentication
 		protected Boolean doInBackground(String... params) {
 			String user = params[0];
 			String pass = params[1];
@@ -104,6 +83,7 @@ public class LoginActivity extends Activity implements OnClickListener {
 			}
 		}
 		
+		// On callback - show toast if failed / go to main screen
 		protected void onPostExecute(Boolean result) {
 			final Context c = getApplicationContext();
 			if (mBusy != null && mBusy.isShowing()) 
