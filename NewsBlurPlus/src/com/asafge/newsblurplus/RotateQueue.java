9d8e6d6fa81f6f71c98cab59be996ddae82fb3fa
asafge <asafge@gmail.com>
2013-08-23 17:59:27 +0300

    Data structure for rotating the hashes

---
+++ NewsBlurPlus/src/com/asafge/newsblurplus/RotateQueue.java
@@ -0,0 +1,43 @@
+package com.asafge.newsblurplus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.text.TextUtils;
+
+/*
+ * A data structure for holding N objects in a list, discarding the last one when it fills up.
+ * Elements should allow serialization to/from Strings.
+ */
+public class RotateQueue<E> {
+	public List<E> Elements;
+	public int Taken;
+	
+	@SuppressWarnings("unchecked")
+	public RotateQueue(int capacity, String serialized) {
+		Elements = new ArrayList<E>(capacity);
+		if (!TextUtils.isEmpty(serialized)) {
+			String[] values = serialized.split(",", capacity);
+			for (String v : values)
+				this.AddElement((E)v);
+		}
+		return;
+	}
+	
+	public void AddElement(E value) {
+		if (Taken == Elements.size())
+			Elements.remove(Elements.size());
+		Elements.add(value);
+	}
+	
+	public boolean SearchElement(E value) {
+		return Elements.contains(value);
+	}
+	
+	public String ToString() {
+		StringBuilder sb = new StringBuilder();
+		for (E element : Elements)
+			sb.append(element.toString()).append(",");
+		return sb.toString();
+	}
+}
