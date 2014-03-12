4f7917ddd04f916a0f414b60de04f6f808365e17
Asaf Geva <asaf@purplebit.com>
2014-03-12 12:27:28 +0200

    RotateQueue cleaups

--- NewsBlurPlus/src/com/asafge/newsblurplus/RotateQueue.java
+++ NewsBlurPlus/src/com/asafge/newsblurplus/RotateQueue.java
@@ -3,38 +3,40 @@ package com.asafge.newsblurplus;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.text.TextUtils;
-
 /*
- * A data structure for holding N objects in a list, discarding the last one when it fills up.
+ * A data structure for holding N objects in a list, discarding the first ones when it fills up.
  * Elements should allow serialization to/from Strings.
  */
 public class  RotateQueue<E> {
 	public List<E> Elements;
 	public int Capacity;
+    private static double FREE_SIZE = 0.05;
 
-	@SuppressWarnings("unchecked")
+    // Constructor, creates a RotateQueue from a comma delimited String.
+    @SuppressWarnings("unchecked")
 	public RotateQueue(int capacity, String serialized) {
 		Elements = new ArrayList<E>(capacity);
 		Capacity = capacity;
-		if (!TextUtils.isEmpty(serialized)) {
+		if (serialized.trim().length() != 0) {
 			String[] values = serialized.split(",", capacity);
 			for (String v : values)
-				if (!TextUtils.isEmpty(v))
+				if (v.trim().length() != 0)
 					this.AddElement((E)v);
 		}
 	}
-	
+
+    // Add a new element to the queue, clean-up old elements if needed
 	public synchronized void AddElement(E value) {
 		if (Capacity == Elements.size()) {
-            int deleteFrom = (int)Math.ceil(Capacity * 0.05);
-            Elements = Elements.subList(deleteFrom, Capacity);
+            int deleteUntil = (int)Math.floor(Capacity * FREE_SIZE);
+            Elements = Elements.subList(deleteUntil, Capacity);
         }
 		if (!this.SearchElement(value)) {
 			Elements.add(value);
         }
 	}
-	
+
+    // Search an element by value
 	public synchronized boolean SearchElement(E value) {
 		return Elements.contains(value);
 	}
